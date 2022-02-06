e66186e4ce693c397ae7d001d53e2f9d208d075f
lucas <lucasgominhaa448@gmail.com>
2022-02-05 23:54:53 -0300

    added final class shopping cart

--- src/srp/shopping-cart.ts
+++ src/srp/shopping-cart.ts
@@ -1,7 +1,9 @@
 type CartItem = { name: string; price: number };
+type OrderStatus = 'open' | 'closed';
 
 export class ShoppingCart {
   private readonly _items: CartItem[] = [];
+  private _orderStatus: OrderStatus = 'open';
 
   addItem(item: CartItem): void {
     this._items.push(item);
@@ -15,10 +17,40 @@ export class ShoppingCart {
     return this._items;
   }
 
+  get orderStatus(): OrderStatus {
+    return this._orderStatus;
+  }
+
   total(): number {
     return +this._items
       .reduce((total, next) => total + next.price, 0)
       .toFixed(2);
+  }
+
+  checkout(): void {
+    if (this.isEmpty()) {
+      console.log('Your shopping cart is empty!');
+      return;
+    }
+
+    this.saveOrder();
+    this.clear();
+  }
+
+  isEmpty(): boolean {
+    return this._items.length === 0;
+  }
+
+  sendMessage(message: string): void {
+    console.log('message sended', message);
+  }
+
+  saveOrder(): void {
+    console.log('your request was saved');
+  }
+
+  clear(): void {
+    this._items.length = 0;
   }
 }
 
@@ -29,3 +61,4 @@ car1.addItem({ name: 'Candle', price: 9.99 });
 
 console.log(car1.items);
 console.log(car1.total());
+console.log(car1.orderStatus);
